e9f62824c791c308a65d8af9e6ab319571a671c5
Will Rose <will@willrr.co.uk>
2024-03-30 00:01:45 +0000

    2

---
+++ 2/2.py
@@ -0,0 +1,16 @@
+
+
+class Game:
+    def __init__(self, )
+
+print("Enter question: ")
+lines = []
+x = ""  # The string is declared
+for line in iter(input, x):
+    lines.append(line)
+    pass
+
+# print(lines)
+
+result = 0
+for answer in lines:
